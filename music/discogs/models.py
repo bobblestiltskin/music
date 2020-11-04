6afb034dc046c84720bce12bb87ec164b189ee81
Bob Wilkinson <bob@fourtheye.org>
2020-11-04 20:36:30 +0000

    django files, project music, app is discogs

---
+++ music/discogs/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Artist(models.Model):
+    artist = models.CharField(max_length=200)
+    def __str__(self):
+        return self.artist
+
+class Format(models.Model):
+    format = models.CharField(max_length=200)
+    def __str__(self):
+        return self.format
+
+class Label(models.Model):
+    label = models.CharField(max_length=200)
+    def __str__(self):
+        return self.label
+
+class Item(models.Model):
+    catalogue_number = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    format = models.ForeignKey(Format, on_delete=models.CASCADE)
+    released = models.DateTimeField('date released')
+    release_id = models.IntegerField(default=0)
