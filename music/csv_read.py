dc9db43c035d8b25c1f86ed12424401d80b429a1
Bob Wilkinson <bob@fourtheye.org>
2020-11-05 20:42:20 +0000

    this now reads the csv_file, normalises the data and populates 4 SQLite tables

--- music/csv_read.py
+++ music/csv_read.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 #
 '''
+this program takes as input a csv file which is generated from discogs.com collection at https://www.discogs.com/users/export?w=collection
+and populates an SQLite database comprising 4 tables : "discogs_artist", "discogs_format", "discogs_label", "discogs_item"
+
+the tables are cleaned prior to insertion - the items table uses the other 3 tables as foreign keys
+
+django was used to create the tables - see https://github.com/bobblestiltskin/music/blob/main/music/discogs/models.py
+
 discogs csv file contains the following fields
 
 Catalog# => WR007, ONUDP16, ON UDP16
@@ -20,13 +27,38 @@ Collection Notes =>
 '''
 
 import csv
+import re
+import sqlite3
 import sys
 
 def display(istring, collection):
-  for a in sorted(collection):
-    print("%s : %s" % (istring, a))
+#  for a in sorted(collection):
+#    print("%s : %s" % (istring, a))
   print("Number of %ss is %d" % (istring, len(collection)))
 
+def clean_tables(cur, db):
+  dlt = "delete from %s" % db
+  cur.execute(dlt)
+  upd = "UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = '%s'" % db
+  cur.execute(upd)
+
+def clean_db(cur):
+  clean_tables(cur, "discogs_artist")
+  clean_tables(cur, "discogs_format")
+  clean_tables(cur, "discogs_label")
+  clean_tables(cur, "discogs_item")
+
+def insert(cur, db, field, collection):
+  for a in sorted(collection):
+    ins = "insert into %s(%s) values(?)" % (db, field)
+    cur.execute(ins, (a,))
+
+def get_id(cur, db, field, value):
+  get = "select id from %s where %s=?" % (db, field)
+  cur.execute(get, (value,))
+  row = cur.fetchone()
+  return row[0]
+ 
 # read the csv file passed as the first argument
 
 csv_file = sys.argv[1]
@@ -38,7 +70,9 @@ obj=csv.DictReader(csvfile)
 artists = set()
 formats = set()
 labels = set()
+items = []
 for row in obj:
+  items.append(row)
   d=dict(row)
   for k in d.keys():
 #    print ("%s => %s" % (k,d[k]))
@@ -48,9 +82,36 @@ for row in obj:
       formats.add(d[k])
     elif k == "Label":
       labels.add(d[k])
-#    elif k == "release_id":
-#      print ("%s => %s" % (k,d[k]))
+
+display("label", labels)
+display("artist", artists)
+display("format", formats)
+
+conn = sqlite3.connect('./db.sqlite3')
+print('Connected to database successfully.')
+cur = conn.cursor()
+
+clean_db(cur)
+
+insert(cur, "discogs_artist", "artist", artists)
+insert(cur, "discogs_format", "format", formats)
+insert(cur, "discogs_label", "label", labels)
 #
-#display("label", labels)
-#display("artist", artists)
-#display("format", formats)
+# now we insert the normalised data into the item table
+#
+for row in items:
+  d=dict(row)
+  for k in d.keys():
+    if k == "Artist":
+      art_id = get_id(cur, "discogs_artist", "artist", d[k])
+    elif k == "Format":
+      for_id = get_id(cur, "discogs_format", "format", d[k])
+    elif k == "Label":
+      lab_id = get_id(cur, "discogs_label", "label", d[k])
+  ins = "insert into discogs_item(catalogue_number, title, released, release_id, artist_id, format_id, label_id) values(?, ?, ?, ?, ?, ?, ?)"
+  vals = (d["Catalog#"], d["Title"], d["Released"], d["release_id"], art_id, for_id, lab_id)
+  cur.execute(ins, vals)
+
+print('inserted to database successfully.')
+conn.commit()
+conn.close()
